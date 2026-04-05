030e86d26cd2aa08ab667be2f891b098c5155f4e
nguyenkimnhu <nguyenthikimnhu7979@gmail.com>
2026-04-05 23:32:31 +0700

    Thêm chức năng xem chi tiết mentor

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-// src/App.jsx
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import MentorListPage from './MentorListPage'
+import MentorDetail from './MentorDetail'
+import MentorReview from './MentorReview'
 
 function App() {
-  return <MentorListPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MentorListPage />} />
+        <Route path="/mentor/:id" element={<MentorDetail />} />
+        <Route path="/mentor/:id/reviews" element={<MentorReview />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
